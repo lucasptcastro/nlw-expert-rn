d85b39115f1b962a1cdf169ebf0ec2f52348f5ee
lucasptcastro <lucasptcastro@gmail.com>
2024-02-11 11:29:24 -0300

    ✨ feat: created remove and clear functions

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from "./helpers/cart-in-memory";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export interface IProductCart extends ProductProps {
   quantity: number;
@@ -9,12 +11,29 @@ export interface IProductCart extends ProductProps {
 interface IState {
   products: IProductCart[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
+  clear: () => void;
 }
 
-export const useCartStore = create<IState>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
-}));
+export const useCartStore = create(
+  persist<IState>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+
+      clear: () => set(() => ({ products: [] })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
