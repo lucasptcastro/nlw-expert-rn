818ca750b365016acc8e62eac39934cfa8757db8
lucasptcastro <lucasptcastro@gmail.com>
2024-02-11 11:30:32 -0300

    ✨ feat: added redirect to home

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/Button";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
 import { Image, Text, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/LinkButton";
@@ -12,11 +12,17 @@ export default function Product() {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddToCart() {
-    carStore.add(product);
-    navigation.goBack();
+    if (product) {
+      carStore.add(product!);
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -28,6 +34,7 @@ export default function Product() {
       />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
